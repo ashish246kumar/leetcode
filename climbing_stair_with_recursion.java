9e0ee673948763b7a108252b08dade8487232237
ashish246kumar <89826676+ashish246kumar@users.noreply.github.com>
2022-01-14 22:38:25 +0530

    Add files via upload

---
+++ climbing_stair_with_recursion.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n<0)
+        {
+            return 0;
+        }
+        if(n==0)
+        {
+            return 1;
+        }
+        return climbStairs(n-2)+climbStairs(n-1);
+    }
+}
