a885e0a8b7d5bed346ee9cd8b86b2cdef4e20fea
ashish246kumar <89826676+ashish246kumar@users.noreply.github.com>
2022-01-21 08:50:44 +0530

    Add files via upload

---
+++ 350_intersection_of_two_arrays_II.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+          HashMap<Integer,Integer>h1=new HashMap<>();
+        int minlen=0,len1=nums1.length,len2=nums2.length,k=0;
+         for(int val:nums1)
+        {
+             if(h1.containsKey(val))
+             {
+               h1.put(val,h1.get(val)+1);
+             }
+             else{
+                h1.put(val,1);
+            }
+        }
+         for(int val2:nums2)
+         {
+             if(h1.containsKey(val2)&&h1.get(val2)>0)
+             {
+                
+                 nums1[k++]=val2;
+                
+                 
+                 h1.put(val2,h1.get(val2)-1);
+             }
+         }
+         int[] nums4=new int[k];
+        for(int j=0;j<k;j++)
+        {
+            
+            nums4[j]=nums1[j];
+        }
+        return nums4;
+    }
+}
