a9f8ca154d4069c3bc8d0cea4e67165e9c000cb9
ashish246kumar <89826676+ashish246kumar@users.noreply.github.com>
2022-01-23 20:40:38 +0530

    Add files via upload

---
+++ pattern2.java
@@ -0,0 +1,34 @@
+package developmentPackage;
+
+public class LoopPattern {
+
+	public static void main(String[] args) {
+		int j,i=1,n=4,k=1,a=0;
+		char ch='A';
+		while(i<=n)
+		{
+			
+
+			j=1;
+
+			while(j<=n)
+			{
+				System.out.print(ch+" ");
+
+				j++;
+				 a=ch;
+				ch=(char)(a+1);
+			}
+
+			System.out.println();
+			i++;
+			
+		}
+
+	}
+
+}
+output
+A B C
+D E F
+G H I
