e7efb860ab4b525074bc1d6835336c72de7fa470
ashish246kumar <89826676+ashish246kumar@users.noreply.github.com>
2022-04-04 19:35:05 +0530

    Add files via upload

---
+++ 374_GuessNumberHigherOrLower.java
@@ -0,0 +1,22 @@
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int lo=1,hi=n;
+        while(lo<=hi)
+        {
+            int mid=lo+(hi-lo)/2;
+            if(guess(mid)==0)
+            {
+                return mid;
+            }
+            else if(guess(mid)==-1)
+            {
+                hi=mid-1;
+            }
+            else if(guess(mid)==1){
+                lo=mid+1;
+            }
+            
+        }
+        return -1;
+    }
+}
